de1d40918b2dbb2ceaa05590ced6d593e754c338
dinukailangarathna <65271949+dinukailangarathna@users.noreply.github.com>
2023-11-03 12:57:10 +0530

    Create install.sh

---
+++ install/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pip install scapy
+sudo apt-get install python-scipy -y
+pip install tldextract
+pip install -e git://github.com/philarkwright/Alexa-Top-Sites.git#egg=alexa-top-sites
+pip install pushbullet.py
+
