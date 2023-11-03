b12e406eff0e3aa9fec60987803002f2ef32619c
dinukailangarathna <65271949+dinukailangarathna@users.noreply.github.com>
2023-11-03 12:57:39 +0530

    Delete install/install.sh

--- install/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install scapy
-sudo apt-get install python-scipy -y
-pip install tldextract
-pip install -e git://github.com/philarkwright/Alexa-Top-Sites.git#egg=alexa-top-sites
-pip install pushbullet.py
-
